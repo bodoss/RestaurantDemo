ad1e7fd693474545e9632166e74675f0d47aaf00
Bohdan Trofymchuk <bohdan.trofymchuk@gmail.com>
2019-12-13 20:28:34 +0200

    init project

---
+++ app/src/main/java/com/bodoss/restaurantdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bodoss.restaurantdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
